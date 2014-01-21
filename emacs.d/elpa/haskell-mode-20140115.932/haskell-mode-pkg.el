d2548aee852368af29de02dc4e650a0bf2b34094
sorpa'as plat <sorpaas@gmail.com>
2014-01-21 15:32:17 +0800

    backup

---
+++ emacs.d/elpa/haskell-mode-20140115.932/haskell-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "haskell-mode" "20140115.932" "A Haskell editing mode" 'nil)
--- emacs.d/org-clock-save.el
+++ emacs.d/org-clock-save.el
@@ -1,1 +1,3 @@
-;; org-persist.el - Brendans-MacBook-Pro.local at <2014-01-05 Sun 17:28>
+;; org-persist.el - Brendans-MacBook-Pro.local at <2014-01-21 Tue 14:35>
+(setq resume-clock '("/Users/sorpaas/Workspace/org/notes.org" . 5209))
+(setq stored-clock-history '(("/Users/sorpaas/Workspace/org/notes.org" . 423) ("/Users/sorpaas/Workspace/org/notes.org" . 199) ("/Users/sorpaas/Workspace/org/track/projects.org" . 6286) ("/Users/sorpaas/Workspace/mcm2014/mcm2014.org" . 28) ("/Users/sorpaas/Workspace/org/notes.org" . 73) ("/Users/sorpaas/Workspace/org/notes.org" . 4826) ("/Users/sorpaas/Workspace/org/notes.org" . 5094)))
